54f17470d05be827e77370dc0559405a10f3bd6d
Cris Bui <thaotao768@gmail.com>
2026-06-23 10:08:06 +0700

    bai3

--- src/main.java
+++ src/main.java
@@ -3,16 +3,14 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int firstNumber, secondNumber;
-        System.out.print("Nhập số thứ nhất: ");
-        firstNumber = sc.nextInt();
-        System.out.print("Nhập số thứ hai: ");
-        secondNumber = sc.nextInt();
-        System.out.println("--- Kết quả ---");
-        System.out.printf("Tổng = %d\n", firstNumber + secondNumber);
-        System.out.printf("Hiệu = %d\n", firstNumber - secondNumber);
-        System.out.printf("Tích = %d\n", firstNumber * secondNumber);
-        System.out.printf("Thương = %d\n", firstNumber / secondNumber);
-        System.out.printf("Phần dư = %d", firstNumber % secondNumber);
+        int ts1, ms1, ts2, ms2;
+        System.out.println("Nhập lần luot tử số mà mẫu số của 2 phân số: ");
+        ts1 = sc.nextInt();
+        ms1 = sc.nextInt();
+        ts2 = sc.nextInt();
+        ms2 = sc.nextInt();
+        int ts = ts1 * ms2 + ts2 * ms1;
+        int ms = ms1 * ms2;
+        System.out.printf("Tổng 2 phân số đã cho là: %d/%d", ts, ms);
     }
 }
